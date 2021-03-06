e66aeeba5a393745b3d1e9bf0b0cabf156243cf8
willian arcaya <willianarcaya13@pop-os.localdomain>
2021-03-06 12:10:16 -0300

    1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        main: "#2A506F",
+        greenW: "#A5DE37",
+        grayW: "#CECECE"
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
